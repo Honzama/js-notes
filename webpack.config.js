aff4449c2de4877aa65b07e02f10545a773eaeb7
Jan Macek <honzama1996@seznam.cz>
2019-06-01 17:26:36 +0200

    Config setup

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     output: {
-        path: __dirname + '/public',
+        path: '/',
         filename: 'bundle.js',
         publicPath: '/'
     },
