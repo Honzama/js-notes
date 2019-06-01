825f5730b16e178101b16d7efd6377862f00a60b
Jan Macek <honzama1996@seznam.cz>
2019-06-01 15:31:49 +0200

    Update src\index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './app'
 
 const render = Component => {
     ReactDOM.render(
-        <HashRouter>
+        <BrowserRouter>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
         , document.getElementById('root')
     )
 };
